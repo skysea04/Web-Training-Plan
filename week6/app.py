1ecae3671f0a41f6f3db1b2b979bc6d8236bdf93
skysea04 <73434165+skysea04@users.noreply.github.com>
2021-03-29 16:35:52 +0800

    把不重要的註解刪掉

---
+++ week6/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, redirect, request, url_for, session
+from datetime import timedelta
+import os
+from mysql_connect import cursor, db, db_select, db_insert
+
+app = Flask(__name__)
+app.secret_key = os.urandom(16).hex()
+
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1) #控制session作用時間
+
+# 首頁
+@app.route('/')
+def index():
+    if 'username' in session:
+        return redirect(url_for('member'))
+    return render_template('index.html')
+
+# 註冊
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    name = request.values['name']
+    username = request.values['username']
+    password = request.values['password']
+
+    user = db_select(username=username)
+    ##如果使用者已存在，回傳錯誤頁面;若不存在則註冊帳號
+    if user:
+        message = '帳號 %s 已經被註冊'%(username)
+        return redirect(url_for('error', message=message))
+    else:
+        db_insert(name=name, username=username, password=password)
+        return redirect(url_for('index'))
+
+# 登入
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    username = request.values['username']
+    password = request.values['password']
+
+    user = db_select(username=username, password=password)
+    # 如果有該user，給session後重新導向到會員頁
+    if user:
+        session['name'] = user['name']
+        session['username'] = user['username']
+        return redirect(url_for('member'))
+    message = '帳號或密碼輸入錯誤'
+    return redirect(url_for('error', message = message))
+
+# 會員頁
+@app.route('/member/')
+def member():
+    if 'username' in session:
+        return render_template('member.html', name=session['name'])
+    return redirect(url_for('index'))
+
+
+# 登出
+@app.route('/signout')
+def logout():
+    # 刪除session
+    session.clear()
+    return redirect(url_for('index'))
+
+# 錯誤頁面
+@app.route('/error/')
+def error():
+    message = request.args.get('message')
+    return render_template('error.html', message=message)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=3000, debug=True)
