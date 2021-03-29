83f109c9ca37438de330bf4848cbb7105e2427d2
skysea04 <73434165+skysea04@users.noreply.github.com>
2021-03-29 17:06:42 +0800

    字串錯誤更新

--- week6/mysql_connect.py
+++ week6/mysql_connect.py
@@ -15,7 +15,7 @@ cursor = db.cursor()
 def db_select(**kargs):
     sql='SELECT * FROM user WHERE '
     for key in kargs:
-        sql += f"{ key } = { kargs[key] } and "
+        sql += f"{ key } = \'{ kargs[key] }\' and "
     sql = sql[:-5]   
     cursor.execute(sql)
     user = cursor.fetchone()
